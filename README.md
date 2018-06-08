1e86e92796d8811fc91797db3a7513907586f95f
Dusty Benac <dustin.benac@foresee.com>
2018-06-08 16:47:25 -0400

    Updating to use application.yml instead of command line props

---
+++ README.md
@@ -0,0 +1,7 @@
+# Engine Data Formatter
+
+To use this application: 
+
+1. Define the parameters in the application.yml (make sure the correct clientId and measurementId are being used)
+2. Put the input file in the resources folder (default name "export.json")
+3. Run the app
---
+++ src/main/java/com/foresee/dataformatter/Application.java
@@ -0,0 +1,38 @@
+package com.foresee.dataformatter;
+
+import com.google.common.base.Stopwatch;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.io.File;
+
+/**
+ * @author dustin.benac
+ * @since 2/13/2017
+ */
+@SpringBootApplication
+@Slf4j
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        ConfigurableApplicationContext ctx = new SpringApplicationBuilder(Application.class)
+                .web(false)
+                .application()
+                .run(args);
+
+        File f = new File("./");
+        System.out.println(f.getAbsolutePath());
+
+        log.debug("Starting...");
+        Stopwatch stopwatch = Stopwatch.createStarted();
+
+        ctx.getBean(DataFormatter.class).execute();
+
+        log.debug("Took {} to complete", stopwatch);
+        ctx.close();
+        System.exit(0);
+    }
+
+}
